cbb6672fbc6e2cb5f33117049f5895a0a58445fc
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-18 17:01:21 +0300

    Create db_storage.py - incomplete

---
+++ models/engine/db_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" defines the DBStorage class """
+
+from os import getenvfrom models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+class DBStorage:
+    """ manages MySQL database """
+    __engine = None
+    __session = None
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """create all tables and create the current database session"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
