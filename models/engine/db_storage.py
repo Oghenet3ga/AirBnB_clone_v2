2553b16d92c771d0247c430262a7fb7ebf67e5d7
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-19 19:02:44 +0300

    Update db_storage.py - for task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ defines the DBStorage class """
 
-from os import getenv
+from os import environ
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
 from models.user import User
 from models.state import State
@@ -14,6 +16,41 @@ class DBStorage:
     """ manages MySQL database """
     __engine = None
     __session = None
+
+    def __init__(self):
+        """create the engine"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(environ.get('HBNB_MYSQL_USER'), 
+                                      environ.get('HBNB_MYSQL_PWD'), 
+                                      environ.get('HBNB_MYSQL_HOST'),
+                                      environ.get('HBNB_MYSQL_DB')),
+                                      pool_pre_ping=True)
+
+        if environ.get('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session)
+
+    def all(self, cls=None):
+        """Query all objects depending on class name"""
+        classes = [User, State, City, Amenity, Place, Review]
+        objects = {}
+
+        if cls:
+            query = self.__session.query(cls).all()
+            for obj in query:
+                key = f"{type(obj).__name__}.{obj.id}"
+                objects[key] = obj
+        else:
+            for cls in classes:
+                query = self.__session.query(cls).all()
+                for obj in query:
+                    key = f"{type(obj).__name__}.{obj.id}"
+                    objects[key] = obj
+
+        return objects
 
     def new(self, obj):
         """add the object to the current database session"""
