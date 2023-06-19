cc471a1b79709ba734ba33f8bfad788fc85fbc4e
Cyril-777 <kyrillosmaher10@gmail.com>
2023-06-19 09:25:23 -0700

    pep8 fixed

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,12 @@
 """This module instantiates an object of class FileStorage"""
 from os import environ
 
+
 if environ.get('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
-else: 
+else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
     storage.reload()
