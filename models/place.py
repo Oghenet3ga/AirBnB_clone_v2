f1bb354f065e52c7f3c74d4cb3282c0e6000ed65
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-21 17:07:38 +0300

    Update place.py - for task 10

--- models/place.py
+++ models/place.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+""" Place Module for HBNB project """ 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-import models
+from sqlalchemy.ext.declarative import declarative_base
+from models.amenity import Amenity
+
+Base = declarative_base()
+
+place_amenity = Table('place_amenity', Base.metadata, 
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+)
 
 class Place(BaseModel, Base):
     """ A place to stay """
-
     __tablename__ = 'places'
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False) 
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0)
+    number_rooms = Column(Integer, default=0) 
     number_bathrooms = Column(Integer, default=0) 
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float, nullable=True)
+    max_guest = Column(Integer, default=0) 
+    price_by_night = Column(Integer, default=0) 
+    latitude = Column(Float, nullable=True) 
     longitude = Column(Float, nullable=True)
+
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
 
     reviews = relationship("Review", backref="place", cascade="all, delete") 
 
-    user = relationship("User", backref="places")
+    user = relationship("User", backref="places") 
     cities = relationship("City", backref="places")
