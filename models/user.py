ace0dd12731b30ce3a3ac1023105afabda442175
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-21 19:01:59 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -2,8 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
-from sqlalchemy.types import String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.review import Review
@@ -11,7 +10,7 @@ from models.review import Review
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
