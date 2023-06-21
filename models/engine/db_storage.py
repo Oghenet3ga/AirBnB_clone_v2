c6c56674b161a510851d97d75cac389bac7c5c01
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-21 19:00:32 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,6 +11,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from sqlalchemy.ext.declarative import declarative_base
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.place import place_amenity
