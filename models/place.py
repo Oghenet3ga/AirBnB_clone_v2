96d386e7a835566016b2e04a1f63e84d48fa31f4
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-21 19:19:19 +0300

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """ 
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+import models
+from os import getenv
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 
-Base = declarative_base()
 
-place_amenity = Table('place_amenity', Base.metadata, 
-    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
-)
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
+    __tablename__ = "places"
 
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -27,10 +32,39 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0) 
     latitude = Column(Float, nullable=True) 
     longitude = Column(Float, nullable=True)
+    amenity_ids = []
 
-    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
 
-    reviews = relationship("Review", backref="place", cascade="all, delete") 
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """returns a list of reviews.id"""
+            var = models.storage.all()
+            list = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    list.append(var[key])
+            for x in list:
+                if (x.place_id == self.id):
+                    result.append(x)
+            return (result)
 
-    user = relationship("User", backref="places") 
-    cities = relationship("City", backref="places")
+        @property
+        def amenities(self):
+            """returns a list of amenity id"""
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """adds amenity ids to the attr"""
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
