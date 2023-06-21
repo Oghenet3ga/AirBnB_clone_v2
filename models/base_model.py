ee2d199034df10942f6c29538ee41cedc519f2a6
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-21 18:53:33 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,8 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, DateTime
-from models import storage_type
+from sqlalchemy import Column, String, DateTime, Integer
+import models
 
 Base = declarative_base()
 
@@ -23,48 +23,47 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
         else:
-            for k in kwargs:
-                if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.fromisoformat(kwargs[k]))
-                elif k != '__class__':
-                    setattr(self, k, kwargs[k])
-            if storage_type == 'db':
-                if not hasattr(kwargs, 'id'):
-                    setattr(self, 'id', str(uuid.uuid4()))
-                if not hasattr(kwargs, 'created_at'):
-                    setattr(self, 'created_at', datetime.now())
-                if not hasattr(kwargs, 'updated_at'):
-                    setattr(self, 'updated_at', datetime.now())
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
         return '[{}] ({}) {}'.format(
-            self.__class__.__name__, self.id, self.__dict__)
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """return str"""
+        return self.__str__()
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.utcnow()
         storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        newdct = self.__dict__.copy()
-        newdct['__class__'] = self.__class__.__name__
-        for i in newdct:
-            if type(newdct[i]) is datetime:
-                newdct[i] = newdct[i].isoformat()
-        if '_sa_instance_state' in newdct.keys():
-            del(newdct['_sa_instance_state'])
-        return newdct
+        new_dict = dict(self.__dict__)
+        new_dict["__class__"] = str(type(self).__name__)
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in new_dict.keys():
+            del new_dict['_sa_instance_state']
+        return new_dict
 
     def delete(self):
         """Delete the current instance from the storage"""
-        from models import storage
-        storage.delete(self)
+        models.storage.delete(self)
