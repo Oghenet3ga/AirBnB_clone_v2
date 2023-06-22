e8fd5fbd219b6ab883195f6fd7a3d8474fb641ab
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-06-22 03:52:27 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ defines the DBStorage class """
 
-from os import getenv
-from models.base_model import Base
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.user import User
 from models.state import State
 from models.city import City
@@ -14,6 +16,28 @@ class DBStorage:
     """ manages MySQL database """
     __engine = None
     __session = None
+
+    def __init__(self):
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'.format(
+            os.environ.get('HBNB_MYSQL_USER'),
+            os.environ.get('HBNB_MYSQL_PWD'),
+            os.environ.get('HBNB_MYSQL_HOST'),
+            os.environ.get('HBNB_MYSQL_DB')
+            ), pool_pre_ping=True)
+        if os.environ.get('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        if cls is not None:
+            instances = self.__session.query(cls).all()
+        else:
+            instances = []
+            for cls in [State, City, User, Place, Review]:
+                instances.extend(self.__session.query(cls).all())
+        all_instances = {}
+        for instance in instances:
+            all_instances['{}.{}'.format(instance.__class__.__name__, instance.id)] = instance
+        return all_instances
 
     def new(self, obj):
         """add the object to the current database session"""
